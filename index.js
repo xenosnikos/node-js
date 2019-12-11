75bf4e92f3584702820fd2d7b4a4d83a424d0be1
Nick Xenos <nxenos@me.com>
2019-12-11 17:54:54 -0500

    node shit

---
+++ index.js
@@ -0,0 +1,7 @@
+//jshint esversion:6
+var superheroes = require("superheroes");
+var myHero =superheroes.random();
+console.log(myHero);
+var supervillains = require("supervillains");
+var myVillain =supervillains.random();
+console.log(myVillain);
